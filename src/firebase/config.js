fbeceaa1f02b8475e225c0f0d5df0ad0ecae7bab
Maciek C <mac.celinski@gmail.com>
2021-02-26 10:01:51 +0100

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyCEd0YR4sxpsh-yhxCq4s793YV1KPr0Icw",
+	authDomain: "firegram-netninja-6dbed.firebaseapp.com",
+	projectId: "firegram-netninja-6dbed",
+	storageBucket: "firegram-netninja-6dbed.appspot.com",
+	messagingSenderId: "589369514784",
+	appId: "1:589369514784:web:b12df30f2069e4ad2b1545",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
