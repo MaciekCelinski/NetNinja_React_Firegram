954cd8be61190a04404ea379c1339c3ba78aab19
Maciek C <mac.celinski@gmail.com>
2021-03-01 11:36:15 +0100

    Modal

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -2,16 +2,20 @@ import React from "react";
 // custom hooks
 import useFirestore from "../hooks/useFirestore";
 
-const ImageGrid = () => {
+const ImageGrid = ({ setSelectedImg }) => {
 	const { docs } = useFirestore("images");
-
-	console.log("docs: ", docs);
 
 	return (
 		<div className="img-grid">
 			{docs &&
 				docs.map((doc) => (
-					<div className="img-wrap" key={doc.id}>
+					<div
+						className="img-wrap"
+						key={doc.id}
+						onClick={() => {
+							setSelectedImg(doc.url);
+						}}
+					>
 						<img src={doc.url} alt="uploaded img" />
 					</div>
 				))}
